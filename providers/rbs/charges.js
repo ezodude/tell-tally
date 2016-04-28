edb316fb1f420c827bc8699c1615bf21b3cd156e
Ezo Saleh <ezo@the-experimenters.com>
2016-04-28 21:42:11 +0100

    Use money values rather than floats.

--- providers/rbs/charges.js
+++ providers/rbs/charges.js
@@ -1,18 +1,22 @@
 'use strict';
 
-const h = require('highland')
-    , _ = require('lodash');
+const h     = require('highland')
+    , _     = require('lodash')
+    , money = require("money-math");
 
 const RBS_CHARGE_TYPES = ['DPC', 'POS', 'S/O', 'D/D'];
 
 const transforms = {
   baseline(c) {
     const source = c[2].split(',')[1];
+    const value = Number.parseFloat(c[3], 10);
+    const normalised = money.floatToAmount(value < 0 ? (-1 * value) : value);
+
     return {
       date: c[0].trim(),
       type: c[1].trim(),
       source: _.isString(source) ? source.toLowerCase().trim() : null,
-      value: Number.parseFloat(c[3], 10)
+      value: normalised
     }
   },
 
