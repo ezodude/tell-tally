19835e6f28cebe2c8f3eed2df48e981a87453f52
Ezo Saleh <ezo@the-experimenters.com>
2016-04-28 21:43:16 +0100

    Tally based on submitted expenses.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const csv           = require('fast-csv')
-    , h             = require('highland')
-    , providers  = require('./providers');
+const _          = require('lodash')
+    , csv        = require('fast-csv')
+    , h          = require('highland')
+    , providers  = require('./providers')
+    , money      = require("money-math");
 
 module.exports = Tally;
 
@@ -12,6 +14,7 @@ function Tally(opts) {
   this.dictionary = opts.dictionary;
   this.startDate = opts.startDate;
   this.provider = providers[opts.provider] || providers.default;
+  this.calculations = [];
 }
 
 Tally.prototype.transactionsFrom = function (file) {
@@ -26,11 +29,25 @@ Tally.prototype.transactionsFrom = function (file) {
 };
 
 Tally.prototype.calculate = function (expense) {
-  console.log('CALCULATE', expense);
+  const pattern = _.values(this.dictionary.get(_.snakeCase(expense))).join('|');
+  const regex = new RegExp(pattern, 'i');
+  const calculation = this.transactions.fork().filter(t => regex.test(t.source)).doto(t => t.expense = expense).collect();
+  this.calculations.push(calculation);
 };
 
-Tally.prototype.tally = function () {
-  this.transactions
-  .doto(console.log)
-  .each(_ => {});
+Tally.prototype.tally = function (cb) {
+  const result = {};
+
+  h(this.calculations)
+  .merge()
+  .each(group => {
+    result[group[0].expense] = _.reduce(group, (sum, n) => money.add(sum, n.value), money.centsToAmount('0'));
+  })
+  .stopOnError(e => cb(e, null))
+  .done( __ => {
+    result.total = _.reduce(_.values(result), (sum, n) => money.add(sum, n), money.centsToAmount('0'));
+    cb(null, result);
+  });
+
+  return this;
 };
