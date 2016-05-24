2dfc5130bc3ca4e442d2ff2c2e4f9d082c7641d1
Ezo Saleh <ezo@the-experimenters.com>
2016-05-24 17:10:05 +0100

    Can tally using charges directly.

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
 'use strict';
 
 const _          = require('lodash')
-    , csv        = require('fast-csv')
     , h          = require('highland')
-    , providers  = require('./providers')
     , moment     = require('moment')
     , money      = require("money-math");
 
 module.exports = Tally;
 
-function Tally(opts) {
-  if (!(this instanceof Tally)) return new Tally(opts);
+function Tally(charges, opts) {
+  if (!(this instanceof Tally)) return new Tally(charges, opts);
   opts = opts || {};
   this.dictionary = opts.dictionary;
   this.fromDate = opts.fromDate;
-  this.provider = providers[opts.provider] || providers.default;
   this.calculations = [];
-}
 
-Tally.prototype.transactionsFrom = function (file) {
-  const stream = csv.fromPath(file, {
-    objectMode: true,
-    headers: true,
-    discardUnmappedColumns: true ,
-    ignoreEmpty: true,
-    trim: true
-  });
-  this.transactions = h(stream).through(this.provider);
-  if(this.fromDate){ this.transactions = this.transactions.reject( t => moment(t.date).isBefore(this.fromDate) ); }
-};
+  this.charges = (h.isStream(charges) ? charges : h( charges ));
+  if(this.fromDate){ this.charges = this.charges.reject( t => moment(t.date).isBefore(this.fromDate) ); }
+}
 
 Tally.prototype.calculate = function (expense) {
   const pattern = _.values(this.dictionary.get(_.snakeCase(expense))).join('|');
   const regex = new RegExp(pattern, 'i');
-  const calculation = this.transactions.fork().filter(t => regex.test(t.source)).doto(t => t.expense = expense).collect();
+  const calculation = this.charges.fork().filter(t => regex.test(t.source)).doto(t => t.expense = expense).collect();
   this.calculations.push(calculation);
 };
 
